4f0663b740db154be5639dbace6408ad05bb338c
Isaac Dawson <isaac.dawson@linkai.io>
2019-03-09 15:53:36 +0900

    fix deleteorg function

--- am_migrations/00032_webtech_tables.sql
+++ am_migrations/00032_webtech_tables.sql
@@ -1,5 +1,27 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION am.delete_org(org_id integer) RETURNS void as 
+$BODY$
+BEGIN 
+    delete from am.ladon_role_member where organization_id=org_id;
+    delete from am.ladon_role where organization_id=org_id;
+    delete from am.scan_group_activity where organization_id=org_id;
+    delete from am.web_technologies where organization_id=org_id;
+    delete from am.web_snapshots where organization_id=org_id;
+    delete from am.web_responses where organization_id=org_id;
+    delete from am.web_certificates where organization_id=org_id;
+    delete from am.scan_group_findings where organization_id=org_id;
+    delete from am.scan_group_addresses where organization_id=org_id;
+    delete from am.scan_group_events where organization_id=org_id;
+    delete from am.scan_group where organization_id=org_id;
+    delete from am.users where organization_id=org_id; 
+    delete from am.organizations where organization_id=org_id;
+    
+END
+$BODY$ LANGUAGE plpgsql SECURITY DEFINER;
+-- +goose StatementEnd
+
 create table am.web_techtypes (
     techtype_id serial primary key,
     techname varchar(512) not null default '',
@@ -13,11 +35,14 @@ create table am.web_techtypes (
 
 create table am.web_technologies (
     tech_id bigserial not null primary key,
+    organization_id integer REFERENCES am.organizations (organization_id),
+    scan_group_id bigint REFERENCES am.scan_group (scan_group_id),
     snapshot_id bigint REFERENCES am.web_snapshots (snapshot_id),
-    category_id int references am.web_techtypes (techtype_id),
+    techtype_id int references am.web_techtypes (techtype_id),
     matched_text text not null default '',
     match_location text not null default '',
-    version varchar(512) not null default ''
+    version varchar(512) not null default '',
+    UNIQUE(snapshot_id,techtype_id,match_location)
 );
 
 grant select, insert, update, delete on am.web_technologies to webdataservice;
@@ -25,6 +50,28 @@ grant select on am.web_techtypes to webdataservice;
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION am.delete_org(org_id integer) RETURNS void as 
+$BODY$
+BEGIN 
+    delete from am.ladon_role_member where organization_id=org_id;
+    delete from am.ladon_role where organization_id=org_id;
+    delete from am.scan_group_activity where organization_id=org_id;
+    delete from am.web_snapshots where organization_id=org_id;
+    delete from am.web_responses where organization_id=org_id;
+    delete from am.web_certificates where organization_id=org_id;
+    delete from am.scan_group_findings where organization_id=org_id;
+    delete from am.scan_group_addresses where organization_id=org_id;
+    delete from am.scan_group_events where organization_id=org_id;
+    delete from am.scan_group where organization_id=org_id;
+    delete from am.users where organization_id=org_id; 
+    delete from am.organizations where organization_id=org_id;
+
+END
+$BODY$ LANGUAGE plpgsql SECURITY DEFINER;
+-- +goose StatementEnd
+
 revoke select, insert, update, delete on am.web_technologies from webdataservice;
 revoke select on am.web_techtypes from webdataservice;
 drop table am.web_technologies;
