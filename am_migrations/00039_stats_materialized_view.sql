e94422c35e1b118efb1e96ed8fd1b543fc954d81
Isaac Dawson <isaac.dawson@linkai.io>
2019-05-15 12:49:59 +0900

    fix matview to group by scangroup

--- am_migrations/00039_stats_materialized_view.sql
+++ am_migrations/00039_stats_materialized_view.sql
@@ -8,7 +8,7 @@ select wb.organization_id, wb.scan_group_id, agg.server, count(1) as cnt from
 			where load_host_address=host_address 
 			and load_ip_address=ip_address 
 			and url_request_timestamp > now()-'7 days'::interval
-			group by server,host_address) as agg 
+			group by scan_group_id,server,host_address) as agg 
 join am.web_responses as wb  on wb.url_request_timestamp=agg.url_request_timestamp and wb.response_timestamp=agg.response_timestamp 
 join am.scan_group as sg on sg.scan_group_id=wb.scan_group_id where sg.deleted=false 
 group by wb.organization_id,wb.scan_group_id,agg.server order by cnt desc;
